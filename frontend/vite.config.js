2452c76754eecaccfd05198e172a5f6d5cb0b9f3
unknown <rizsameer001@gmail.com>
2026-03-01 02:13:20 +0530

    env updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,22 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: isDev
+            ? 'http://localhost:5000'
+            : 'https://ecom-9ygt.onrender.com',
+          changeOrigin: true,
+          secure: true,
+        },
       },
     },
-  },
+  };
 });
