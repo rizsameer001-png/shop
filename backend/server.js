5d25d35d5c18f4a38a799613badc42f7884cea43
unknown <rizsameer001@gmail.com>
2026-03-02 06:10:21 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -41,19 +41,23 @@ app.use(cookieParser());
 //   credentials: true,
 // }));
 
-const allowedOrigins = process.env.FRONTEND_URLS
-  ? process.env.FRONTEND_URLS.split(',')
-  : [];
+// const allowedOrigins = process.env.FRONTEND_URLS
+//   ? process.env.FRONTEND_URLS.split(',')
+//   : [];
+
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+// }));
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+  origin: "*",
 }));
 
 // Routes
